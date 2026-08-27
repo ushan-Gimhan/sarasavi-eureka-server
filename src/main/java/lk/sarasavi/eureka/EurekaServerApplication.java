61fb76686e5522fa14cfe0680d25320cb86c3cc0
ushan-Gimhan <ushangimhan@gmail.com>
2026-08-27 16:03:59 +0530

    feat: add student info (S.K.M Ushan Gimhan 241711098), GCP project config, updated READMEs and media/CORS config

--- src/main/java/lk/sarasavi/eureka/EurekaServerApplication.java
+++ src/main/java/lk/sarasavi/eureka/EurekaServerApplication.java
@@ -1,20 +1,8 @@
 package lk.sarasavi.eureka;
 
-import com.netflix.appinfo.ApplicationInfoManager;
-import com.netflix.discovery.EurekaClientConfig;
-import com.netflix.eureka.EurekaServerConfig;
-import com.netflix.eureka.cluster.PeerEurekaNodes;
-import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
-import com.netflix.eureka.resources.ServerCodecs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration.RefreshablePeerEurekaNodes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -22,24 +10,5 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
-    }
-
-    @Bean
-    @Primary
-    public PeerEurekaNodes peerEurekaNodes(PeerAwareInstanceRegistry registry,
-                                          EurekaServerConfig serverConfig,
-                                          EurekaClientConfig clientConfig,
-                                          ServerCodecs serverCodecs,
-                                          ApplicationInfoManager applicationInfoManager) {
-        return new RefreshablePeerEurekaNodes(registry, serverConfig, clientConfig, serverCodecs, applicationInfoManager) {
-            @Override
-            protected List<String> resolvePeerUrls() {
-                return Arrays.asList(
-                    "http://vm-node-a.platform:8761/eureka/",
-                    "http://vm-node-b.platform:8761/eureka/",
-                    "http://vm-node-c.platform:8761/eureka/"
-                );
-            }
-        };
     }
 }
